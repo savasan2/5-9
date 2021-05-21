8e52efcc5c1923fe1c52f51acf18f28c72a9abc5
savasan2 <83535755+savasan2@users.noreply.github.com>
2021-05-21 06:19:49 +0000

    finish5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'd70718af779bcebbfedadf6b645633728757554e5e54ec936e0793715b4016db4d9f89b66f150e3dfcb9c83eb9d4043a14e328cb6585cc13b7ce0e2620126371'
